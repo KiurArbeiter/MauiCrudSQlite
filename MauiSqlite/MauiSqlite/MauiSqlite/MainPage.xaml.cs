df0ea132d79d557b9a2aa63f16824962c5604df3
KiurArbeiter <127192996+KiurArbeiter@users.noreply.github.com>
2024-06-07 06:47:27 -0400

    MainPage height ja width muudatus
    height ja width muudatus ja kaasnev error fix

--- MauiSqlite/MauiSqlite/MauiSqlite/MainPage.xaml.cs
+++ MauiSqlite/MauiSqlite/MauiSqlite/MainPage.xaml.cs
@@ -9,6 +9,12 @@ public partial class MainPage : ContentPage
     {
         InitializeComponent();
         _dbService = dbService;
+
+        // Set the dimensions of the main layout container
+        MainLayout.WidthRequest = 540;
+        MainLayout.HeightRequest = 1000;
+
+        // Load the customers asynchronously
         Task.Run(async () => listView.ItemsSource = await _dbService.GetCustomers());
     }
 
@@ -17,7 +23,6 @@ public partial class MainPage : ContentPage
         if (_editCustomerId == 0)
         {
             // Add Customer
-
             await _dbService.Create(new Customer
             {
                 CustomerName = nameEntryField.Text,
@@ -28,7 +33,6 @@ public partial class MainPage : ContentPage
         else
         {
             // Edit Customer
-
             await _dbService.Update(new Customer
             {
                 Id = _editCustomerId,
@@ -40,10 +44,12 @@ public partial class MainPage : ContentPage
             _editCustomerId = 0;
         }
 
+        // Clear the input fields
         nameEntryField.Text = string.Empty;
         emailEntryField.Text = string.Empty;
         mobileEntryField.Text = string.Empty;
 
+        // Refresh the list view
         listView.ItemsSource = await _dbService.GetCustomers();
     }
 
@@ -55,91 +61,15 @@ public partial class MainPage : ContentPage
         switch (action)
         {
             case "Edit":
-
                 _editCustomerId = customer.Id;
                 nameEntryField.Text = customer.CustomerName;
                 emailEntryField.Text = customer.Email;
                 mobileEntryField.Text = customer.Mobile;
-
                 break;
-            case "Delete":
 
+            case "Delete":
                 await _dbService.Delete(customer);
                 listView.ItemsSource = await _dbService.GetCustomers();
-
-                break;
-        }
-    }
-}
-namespace MauiSqliteDemo;
-
-public partial class MainPage : ContentPage
-{
-    private readonly LocalDbService _dbService;
-    private int _editCustomerId;
-
-    public MainPage(LocalDbService dbService)
-    {
-        InitializeComponent();
-        _dbService = dbService;
-        Task.Run(async () => listView.ItemsSource = await _dbService.GetCustomers());
-    }
-
-    private async void saveButton_Clicked(object sender, EventArgs e)
-    {
-        if (_editCustomerId == 0)
-        {
-            // Add Customer
-
-            await _dbService.Create(new Customer
-            {
-                CustomerName = nameEntryField.Text,
-                Email = emailEntryField.Text,
-                Mobile = mobileEntryField.Text
-            });
-        }
-        else
-        {
-            // Edit Customer
-
-            await _dbService.Update(new Customer
-            {
-                Id = _editCustomerId,
-                CustomerName = nameEntryField.Text,
-                Email = emailEntryField.Text,
-                Mobile = mobileEntryField.Text
-            });
-
-            _editCustomerId = 0;
-        }
-
-        nameEntryField.Text = string.Empty;
-        emailEntryField.Text = string.Empty;
-        mobileEntryField.Text = string.Empty;
-
-        listView.ItemsSource = await _dbService.GetCustomers();
-    }
-
-    private async void listView_ItemTapped(object sender, ItemTappedEventArgs e)
-    {
-        var customer = (Customer)e.Item;
-        var action = await DisplayActionSheet("Action", "Cancel", null, "Edit", "Delete");
-
-        switch (action)
-        {
-            case "Edit":
-
-                _editCustomerId = customer.Id;
-                nameEntryField.Text = customer.CustomerName;
-                emailEntryField.Text = customer.Email;
-                mobileEntryField.Text = customer.Mobile;
-
-                break;
-            case "Delete":
-
-                await _dbService.Delete(customer);
-                listView.ItemsSource = await _dbService.GetCustomers();
-
                 break;
         }
     }
